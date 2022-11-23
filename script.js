f5a470c302390778eff9cdac0ff004b8e75cc5fa
MrBurp <andreass.ziegler@gmail.com>
2022-11-23 17:51:05 +0100

    fn changeImage working

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function run() {
   index++;
   changeImage();
 }
+
+function changeImage() {
+  if (index > img.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = img.length - 1;
+  }
+  imgs.style.transform = `translateX(${-index * 500}px)`;
+}
