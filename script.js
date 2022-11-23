63849cd117d09d8e3c60d04ad9aaed9bc2051298
MrBurp <andreass.ziegler@gmail.com>
2022-11-23 17:57:19 +0100

    resetInterval

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ function changeImage() {
   imgs.style.transform = `translateX(${-index * 500}px)`;
 }
 
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
 btnRight.addEventListener("click", () => {
   index--;
   changeImage();
