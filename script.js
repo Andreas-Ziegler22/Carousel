f3caa758e6b977e62406f310912742d3cbd1d180
MrBurp <andreass.ziegler@gmail.com>
2022-11-23 17:40:08 +0100

    pre set for run function

---
+++ script.js
@@ -0,0 +1,9 @@
+const imgs = document.getElementById("imgs");
+const btnLeft = document.getElementById("left");
+const btnRight = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
+
+let index = 0;
+
+let interval = setInterval(run, 2000);
